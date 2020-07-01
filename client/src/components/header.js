f7fc944126eb690d3e239f8fa4e21d75f9bd358c
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-01 23:44:45 +0600

    db connection updated

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -17,7 +17,7 @@ class Header extends Component {
         <div className='navbar-nav nav-item dropdown ml-auto'>
           <a
             className='nav-link dropdown-toggle'
-            href='http://example.com'
+            href='#'
             id='dropdown02'
             data-toggle='dropdown'
             aria-haspopup='true'
@@ -103,31 +103,7 @@ class Header extends Component {
                   Posts
                 </Link>
               </li>
-
-              <li className='nav-item'>
-                <a
-                  className='nav-link'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  href='https://github.com/haichao-yu/react-redux-blog'
-                >
-                  GitHub Repo
-                </a>
-              </li>
             </ul>
-            <form className='form-inline my-2 my-md-0'>
-              <input
-                className='form-control mr-sm-2'
-                type='text'
-                placeholder='Search Post'
-              />
-              <button
-                className='btn btn-outline-success my-2 my-sm-0'
-                type='submit'
-              >
-                Search
-              </button>
-            </form>
             <div className='ml-auto'>{this.renderLinks()}</div>
           </div>
         </div>
